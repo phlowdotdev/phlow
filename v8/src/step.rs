de814fbe5b548a7cb90d805992ac0b9be90de7a0
Philippe Assis <codephilippe@gmail.com>
2025-03-07 18:56:25 -0300

    refactor: update StepOutput and Step structures to use NextStep enum for improved flow control

--- v8/src/step.rs
+++ v8/src/step.rs
@@ -8,8 +8,14 @@ use crate::{condition::Condition, payload::Payload, pipeline::Context, Error};
 pub type StepInnerId = String;
 pub type Output = Value;
 
+pub enum NextStep {
+    Step(StepInnerId),
+    Stop,
+    Next,
+}
+
 pub struct StepOutput {
-    pub(crate) next_step: Option<StepInnerId>,
+    pub(crate) next_step: NextStep,
     pub(crate) output: Option<Output>,
 }
 
@@ -30,7 +36,7 @@ pub(crate) struct Step {
     pub(crate) payload: Option<String>,
     pub(crate) then_case: Option<StepInnerId>,
     pub(crate) else_case: Option<StepInnerId>,
-    pub(crate) return_case: Option<Value>,
+    pub(crate) return_case: Option<String>,
 }
 
 impl Step {
@@ -66,8 +72,24 @@ impl Step {
 
     fn evaluate_payload(&self, context: &Context) -> Result<Option<Output>, Error> {
         if let Some(ref payload) = self.payload {
-            let payload = Payload::new(payload.to_string());
-            let output = Some(payload.evaluate(context).map_err(Error::PayloadError)?);
+            let output = Some(
+                Payload::new(payload.to_string())
+                    .evaluate(context)
+                    .map_err(Error::PayloadError)?,
+            );
+            Ok(output)
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn evaluate_return(&self, context: &Context) -> Result<Option<Output>, Error> {
+        if let Some(ref return_case) = self.return_case {
+            let output = Some(
+                Payload::new(return_case.to_string())
+                    .evaluate(context)
+                    .map_err(Error::PayloadError)?,
+            );
             Ok(output)
         } else {
             Ok(None)
@@ -76,40 +98,62 @@ impl Step {
 
     pub fn execute(&self, context: &Context) -> Result<StepOutput, Error> {
         if let Some(ref condition) = self.condition {
-            let result = condition.evaluate(context)?;
+            if condition.evaluate(context)? {
+                let output = if self.evaluate_payload(context)?.is_some() {
+                    self.evaluate_payload(context)?
+                } else {
+                    None
+                };
 
-            if result {
                 if let Some(ref then_case) = self.then_case {
-                    let output = if self.evaluate_payload(context)?.is_some() {
-                        self.evaluate_payload(context)?
-                    } else {
-                        None
-                    };
-
                     return Ok(StepOutput {
-                        next_step: Some(then_case.clone()),
+                        next_step: NextStep::Step(then_case.clone()),
+                        output,
+                    });
+                } else {
+                    return Ok(StepOutput {
+                        next_step: NextStep::Next,
                         output,
                     });
                 }
             } else {
                 if let Some(ref else_case) = self.else_case {
                     return Ok(StepOutput {
-                        next_step: Some(else_case.clone()),
+                        next_step: NextStep::Step(else_case.clone()),
+                        output: None,
+                    });
+                } else {
+                    return Ok(StepOutput {
+                        next_step: NextStep::Stop,
                         output: None,
                     });
                 }
             }
         }
 
-        let output = if self.evaluate_payload(context)?.is_some() {
-            self.evaluate_payload(context)?
-        } else {
-            None
-        };
+        if let Some(return_case) = self.evaluate_return(context)? {
+            return Ok(StepOutput {
+                next_step: NextStep::Stop,
+                output: Some(return_case),
+            });
+        }
 
-        Ok(StepOutput {
-            next_step: None,
-            output,
-        })
+        if let Some(ref payload) = self.payload {
+            let output = Some(
+                Payload::new(payload.to_string())
+                    .evaluate(context)
+                    .map_err(Error::PayloadError)?,
+            );
+
+            return Ok(StepOutput {
+                next_step: NextStep::Next,
+                output,
+            });
+        }
+
+        return Ok(StepOutput {
+            next_step: NextStep::Next,
+            output: None,
+        });
     }
 }
