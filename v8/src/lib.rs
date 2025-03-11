ad2bfd86725f06ab532740afb5453057f96ccb33
Philippe Assis <codephilippe@gmail.com>
2025-03-10 21:28:37 -0300

    refactor: add macro for simplified V8 value conversion with optional parameters

--- v8/src/lib.rs
+++ v8/src/lib.rs
@@ -6,3 +6,18 @@ mod step_worker;
 mod transform;
 mod v8;
 mod variable;
+
+pub use rhai::Engine;
+pub use v8::V8;
+
+#[macro_export]
+macro_rules! v8 {
+    ($value:expr) => {
+        let engine = $crate::Engine::new();
+        V8::try_from_value(&engine, $value, None)
+    };
+    ($value:expr, $params:expr) => {
+        let engine = $crate::Engine::new();
+        V8::try_from_value(&engine, $value, Some($params))
+    };
+}
