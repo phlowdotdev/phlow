3bd87387fde4a92fec453a6bea41529b62a16c2b
Philippe Assis <codephilippe@gmail.com>
2025-03-30 15:59:35 -0300

    feat: integrate OpenTelemetry context into plugin and main for enhanced tracing

--- test_otlp/src/main.rs
+++ test_otlp/src/main.rs
@@ -1,13 +1,15 @@
 use libloading::Library;
+use sdk::opentelemetry::Context;
 use sdk::otlp::init_tracing_subscriber;
-use sdk::tracing::{dispatcher, info, span, Dispatch, Level, Span};
+use sdk::tracing::{dispatcher, span, Dispatch, Level};
+use sdk::tracing_opentelemetry::OpenTelemetrySpanExt;
 use sdk::valu3::prelude::*;
 use sdk::Package;
 use std::sync::mpsc::Sender;
 use std::sync::Arc;
 use std::thread;
 
-type PluginFn = unsafe extern "C" fn(*mut Span, *const Dispatch, *const Sender<Package>);
+type PluginFn = unsafe extern "C" fn(*const Context, *const Dispatch, *const Sender<Package>);
 
 fn main() {
     let _guard = init_tracing_subscriber().expect("failed to initialize tracing");
@@ -25,43 +27,43 @@ fn main() {
         let dispatch_clone = dispatch.clone();
         let plugin_path = *plugin_path;
         let tx_clone = tx.clone();
+        let context = span_clone.context();
 
         thread::spawn(move || unsafe {
             let lib = Library::new(plugin_path).expect("Falha ao carregar a biblioteca");
             let func: libloading::Symbol<PluginFn> =
                 lib.get(b"plugin").expect("Falha ao obter símbolo");
             func(
-                &span_clone as *const _ as *mut _,
+                Box::into_raw(Box::new(context)) as *const Context,
                 Arc::into_raw(dispatch_clone),
                 &tx_clone as *const _ as *const Sender<Package>,
             );
         });
     }
 
-    for package in rx.iter() {
-        println!("Received package: {:?}", package);
+    let handler = thread::spawn(move || {
+        for package in rx.iter() {
+            println!("Received package: {:?}", package);
 
-        let dispatch = package.dispatch.unwrap();
+            let dispatch = package.dispatch.unwrap();
 
-        dispatcher::with_default(&dispatch, || {
-            if let Some(span) = package.span {
-                let parent = span.clone();
-                let _parent_enter = parent.enter();
-                info!("Log do main");
-            }
+            dispatcher::with_default(&dispatch, || {
+                let parent = package.span.unwrap();
 
-            let span = span!(Level::INFO, "receivers");
-            let _enter = span.enter();
+                let span = span!(Level::INFO, "receivers");
+                span.set_parent(parent.context());
+                let _enter = span.enter();
 
-            if let Some(sender) = package.send {
-                let result = sender.send("Hello from main".to_value());
-                match result {
-                    Ok(_) => println!("Sent value from main"),
-                    Err(_) => println!("Failed to send value from main"),
+                if let Some(sender) = package.send {
+                    let result = sender.send("Hello from main".to_value());
+                    match result {
+                        Ok(_) => println!("Sent value from main"),
+                        Err(_) => println!("Failed to send value from main"),
+                    }
                 }
-            }
-        });
-    }
+            });
+        }
+    });
 
-    std::thread::sleep(std::time::Duration::from_secs(3));
+    handler.join().unwrap();
 }
--- tracer/src/lib.rs
+++ tracer/src/lib.rs
@@ -1,15 +1,16 @@
-use std::sync::mpsc::Sender;
-
+use sdk::opentelemetry::Context;
+use sdk::tracing_opentelemetry::OpenTelemetrySpanExt;
 use sdk::{
     otlp::init_tracing_subscriber_plugin,
     tokio::sync::oneshot,
-    tracing::{self, dispatcher, info, span, Dispatch, Level, Span},
+    tracing::{dispatcher, info, span, Dispatch, Level, Span},
     Package,
 };
+use std::sync::mpsc::Sender;
 
 #[no_mangle]
 pub extern "C" fn plugin(
-    span_ptr: *mut Span,
+    context_ptr: *const Context,
     dispatch_ptr: *const Dispatch,
     sender: *const Sender<Package>,
 ) {
@@ -19,21 +20,24 @@ pub extern "C" fn plugin(
         let _guard = init_tracing_subscriber_plugin().expect("failed to initialize tracing");
 
         let dispatch = &*dispatch_ptr;
+        let parent_context = &*context_ptr;
 
         dispatcher::with_default(dispatch, || {
-            let parent = span_ptr.as_ref().unwrap().clone();
-            let _parent_enter = parent.enter();
-
             let span = span!(Level::INFO, "plugin");
+            span.set_parent(parent_context.clone());
             let _enter = span.enter();
+
             execution(sender, span.clone(), dispatch);
             info!("Log do plugin dentro do plugin");
         });
+
+        drop(Box::from_raw(context_ptr as *mut Context));
     }
 }
 
 pub fn execution(sender: &Sender<Package>, span: Span, dispatch: &Dispatch) {
     let (tx, rx) = oneshot::channel();
+    info!("Log antes de enviar");
 
     let package = Package {
         origin: 0,
