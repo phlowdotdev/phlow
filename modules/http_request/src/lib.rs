f13e25daef92f3c6de2f236b7ce8a6428fc03a27
Philippe Assis <codephilippe@gmail.com>
2025-04-04 16:20:49 -0300

    feat: rename http_rest_api to http_server and update related configurations

---
+++ modules/http_request/src/lib.rs
@@ -0,0 +1,28 @@
+use phlow_sdk::{crossbeam::channel, modules::ModulePackage, prelude::*};
+
+plugin_async!(http_request);
+
+pub async fn http_request(setup: ModuleSetup) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let (tx, rx) = channel::unbounded::<ModulePackage>();
+
+    match setup.setup_sender.send(Some(tx)) {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(format!("{:?}", e).into());
+        }
+    };
+
+    for package in rx {
+        match package.sender.send(match package.context.input {
+            Some(value) => value,
+            _ => Value::Null,
+        }) {
+            Ok(_) => {}
+            Err(e) => {
+                tracing::error!("Error sending package: {:?}", e);
+            }
+        }
+    }
+
+    Ok(())
+}
