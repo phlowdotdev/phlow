493069373fc0e5cea0effd8c50fea2fcf20b689c
Philippe Assis <codephilippe@gmail.com>
2025-10-24 17:01:49 -0300

    feat: adicionar suporte a novos corpos de requisição para OpenAI, incluindo Completions, Chat, Images, ImagesEdit, Embeddings e Audio

--- modules/openai/src/config.rs
+++ modules/openai/src/config.rs
@@ -1,109 +1,441 @@
+use phlow_sdk::prelude::*;
 use std::collections::HashMap;
 
-use phlow_sdk::prelude::*;
+#[derive(Debug, Clone, PartialEq)]
+pub enum Role {
+    System,
+    Assistant,
+    User,
+}
 
-/// Configuration for the cache module
-#[derive(Debug, Clone)]
-pub struct OpenaiConfig {
+impl Default for Role {
+    fn default() -> Self {
+        Role::User
+    }
+}
+
+impl FromValueBehavior for Role {
+    type Item = Role;
+
+    fn from_value(value: Value) -> Option<Self::Item> {
+        match value.as_str() {
+            "system" => Some(Role::System),
+            "assistant" => Some(Role::Assistant),
+            "user" => Some(Role::User),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct Message {
+    pub role: Role,
+    pub content: String,
+}
+
+impl FromValueBehavior for Message {
+    type Item = Message;
+
+    fn from_value(value: Value) -> Option<Self::Item> {
+        let role = Role::from_value(value.get("role")?.clone())?;
+        let content = value.get("content")?.to_string();
+
+        Some(Message { role, content })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct CompletionsBody {
     pub model: String,
-    pub max_tokens: Option<usize>,
+    pub prompt: Option<Vec<String>>,
+    pub suffix: Option<String>,
+    pub max_tokens: Option<i32>,
     pub temperature: Option<f32>,
     pub top_p: Option<f32>,
-    pub n: Option<usize>,
+    pub n: Option<i32>,
+    pub stream: Option<bool>,
+    pub logprobs: Option<i32>,
+    pub echo: Option<bool>,
+    pub stop: Option<Vec<String>>,
+    pub presence_penalty: Option<f32>,
+    pub frequency_penalty: Option<f32>,
+    pub best_of: Option<i32>,
+    pub logit_bias: Option<HashMap<String, String>>,
+    pub user: Option<String>,
+}
+
+impl FromValueBehavior for CompletionsBody {
+    type Item = CompletionsBody;
+
+    fn from_value(value: Value) -> Option<Self::Item> {
+        let model = value.get("model")?.to_string();
+
+        let prompt = match value.get("prompt") {
+            Some(p) => {
+                if p.is_array() {
+                    Some(
+                        p.as_array()?
+                            .into_iter()
+                            .map(|s| s.to_string())
+                            .collect::<Vec<String>>(),
+                    )
+                } else {
+                    Some(vec![p.to_string()])
+                }
+            }
+            None => None,
+        };
+
+        let suffix = value.get("suffix").map(|s| s.to_string());
+
+        let max_tokens = value.get("max_tokens").map(|m| m.to_i64().unwrap() as i32);
+
+        let temperature = value.get("temperature").map(|t| t.to_f64().unwrap() as f32);
+
+        let top_p = value.get("top_p").map(|t| t.to_f64().unwrap() as f32);
+
+        let n = value.get("n").map(|n| n.to_i64().unwrap() as i32);
+
+        let stream = value.get("stream").and_then(|s| s.as_bool().cloned());
+
+        let logprobs = value.get("logprobs").map(|l| l.to_i64().unwrap() as i32);
+
+        let echo = value.get("echo").and_then(|e| e.as_bool().cloned());
+
+        let stop = match value.get("stop") {
+            Some(s) => {
+                if s.is_array() {
+                    Some(
+                        s.as_array()?
+                            .into_iter()
+                            .map(|st| st.to_string())
+                            .collect::<Vec<String>>(),
+                    )
+                } else {
+                    Some(vec![s.to_string()])
+                }
+            }
+            None => None,
+        };
+
+        let presence_penalty = value
+            .get("presence_penalty")
+            .map(|p| p.to_f64().unwrap() as f32);
+
+        let frequency_penalty = value
+            .get("frequency_penalty")
+            .map(|f| f.to_f64().unwrap() as f32);
+
+        let best_of = value.get("best_of").map(|b| b.to_i64().unwrap() as i32);
+
+        let logit_bias = match value.get("logit_bias") {
+            Some(lb) => {
+                let mut bias_map = HashMap::new();
+
+                match lb.as_object() {
+                    Some(obj) => {
+                        for (k, v) in obj.iter() {
+                            bias_map.insert(k.to_string(), v.to_string());
+                        }
+                    }
+                    None => return None,
+                }
+
+                Some(bias_map)
+            }
+            None => None,
+        };
+
+        let user = value.get("user").map(|u| u.to_string());
+
+        Some(CompletionsBody {
+            model,
+            prompt,
+            suffix,
+            max_tokens,
+            temperature,
+            top_p,
+            n,
+            stream,
+            logprobs,
+            echo,
+            stop,
+            presence_penalty,
+            frequency_penalty,
+            best_of,
+            logit_bias,
+            user,
+        })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ChatBody {
+    pub model: String,
+    pub messages: Vec<Message>,
+    pub temperature: Option<f32>,
+    pub top_p: Option<f32>,
+    pub n: Option<i32>,
     pub stream: Option<bool>,
     pub stop: Option<Vec<String>>,
+    pub max_tokens: Option<i32>,
     pub presence_penalty: Option<f32>,
     pub frequency_penalty: Option<f32>,
     pub logit_bias: Option<HashMap<String, String>>,
     pub user: Option<String>,
 }
 
-impl Default for OpenaiConfig {
-    fn default() -> Self {
-        Self {
-            model: "gtp-5-mini".to_string(),
-            max_tokens: None,
-            temperature: None,
-            top_p: None,
-            n: None,
-            stream: None,
-            stop: None,
-            presence_penalty: None,
-            frequency_penalty: None,
-            logit_bias: None,
-            user: None,
+impl FromValueBehavior for ChatBody {
+    type Item = ChatBody;
+
+    fn from_value(value: Value) -> Option<Self::Item> {
+        let model = value.get("model")?.to_string();
+
+        let messages_value = value.get("messages")?;
+        let messages_array = messages_value.as_array()?;
+
+        let mut messages = Vec::new();
+        for msg_value in messages_array {
+            let msg = Message::from_value(msg_value.clone())?;
+            messages.push(msg);
         }
+
+        let temperature = value.get("temperature").map(|t| t.to_f64().unwrap() as f32);
+
+        let top_p = value.get("top_p").map(|t| t.to_f64().unwrap() as f32);
+
+        let n = value.get("n").map(|n| n.to_i64().unwrap() as i32);
+
+        let stream = value.get("stream").and_then(|s| s.as_bool().cloned());
+
+        let stop = match value.get("stop") {
+            Some(s) => {
+                if s.is_array() {
+                    Some(
+                        s.as_array()?
+                            .into_iter()
+                            .map(|st| st.to_string())
+                            .collect::<Vec<String>>(),
+                    )
+                } else {
+                    Some(vec![s.to_string()])
+                }
+            }
+            None => None,
+        };
+
+        let max_tokens = value.get("max_tokens").map(|m| m.to_i64().unwrap() as i32);
+
+        let presence_penalty = value
+            .get("presence_penalty")
+            .map(|p| p.to_f64().unwrap() as f32);
+
+        let frequency_penalty = value
+            .get("frequency_penalty")
+            .map(|f| f.to_f64().unwrap() as f32);
+
+        let logit_bias = match value.get("logit_bias") {
+            Some(lb) => {
+                let mut bias_map = HashMap::new();
+
+                match lb.as_object() {
+                    Some(obj) => {
+                        for (k, v) in obj.iter() {
+                            bias_map.insert(k.to_string(), v.to_string());
+                        }
+                    }
+                    None => return None,
+                }
+
+                Some(bias_map)
+            }
+            None => None,
+        };
+
+        let user = value.get("user").map(|u| u.to_string());
+
+        Some(ChatBody {
+            model,
+            messages,
+            temperature,
+            top_p,
+            n,
+            stream,
+            stop,
+            max_tokens,
+            presence_penalty,
+            frequency_penalty,
+            logit_bias,
+            user,
+        })
     }
 }
 
-impl TryFrom<&Value> for OpenaiConfig {
-    type Error = String;
+#[derive(Debug, Clone, PartialEq)]
+pub struct ImagesBody {
+    pub prompt: String,
+    pub n: Option<i32>,
+    pub size: Option<String>,
+    pub response_format: Option<String>,
+    pub user: Option<String>,
+}
 
-    // Value is value3::Value and not serde_json::Value
-    fn try_from(value: &Value) -> Result<Self, Self::Error> {
-        let mut config = OpenaiConfig::default();
+impl FromValueBehavior for ImagesBody {
+    type Item = ImagesBody;
 
-        if let Some(model) = value.get("model") {
-            config.model = model.to_string();
+    fn from_value(value: Value) -> Option<Self::Item> {
+        let prompt = value.get("prompt")?.to_string();
+
+        let n = value.get("n").map(|n| n.to_i64().unwrap() as i32);
+
+        let size = value.get("size").map(|s| s.to_string());
+
+        let response_format = value.get("response_format").map(|r| r.to_string());
+
+        let user = value.get("user").map(|u| u.to_string());
+
+        Some(ImagesBody {
+            prompt,
+            n,
+            size,
+            response_format,
+            user,
+        })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ImagesEditBody {
+    pub image: String,
+    pub mask: Option<String>,
+    pub images_body: ImagesBody,
+}
+
+impl FromValueBehavior for ImagesEditBody {
+    type Item = ImagesEditBody;
+
+    fn from_value(value: Value) -> Option<Self::Item> {
+        let image = value.get("image")?.to_string();
+
+        let mask = value.get("mask").map(|m| m.to_string());
+
+        let images_body_value = value.get("images_body")?;
+        let images_body = ImagesBody::from_value(images_body_value.clone())?;
+
+        Some(ImagesEditBody {
+            image,
+            mask,
+            images_body,
+        })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct EmbeddingsBody {
+    pub model: String,
+    pub input: Vec<String>,
+    pub user: Option<String>,
+}
+
+impl FromValueBehavior for EmbeddingsBody {
+    type Item = EmbeddingsBody;
+
+    fn from_value(value: Value) -> Option<Self::Item> {
+        let model = value.get("model")?.to_string();
+
+        let input_value = value.get("input")?;
+        let input_array = input_value.as_array()?;
+
+        let mut input = Vec::new();
+        for inp in input_array {
+            input.push(inp.to_string());
         }
 
-        if let Some(max_tokens) = value.get("max_tokens") {
-            config.max_tokens = Some(max_tokens.to_u64().unwrap() as usize);
-        }
+        let user = value.get("user").map(|u| u.to_string());
 
-        if let Some(temperature) = value.get("temperature") {
-            config.temperature = Some(temperature.to_f64().unwrap() as f32);
-        }
+        Some(EmbeddingsBody { model, input, user })
+    }
+}
 
-        if let Some(top_p) = value.get("top_p") {
-            config.top_p = Some(top_p.to_f64().unwrap() as f32);
-        }
+#[derive(Debug, Clone, PartialEq)]
+pub struct AudioBody {
+    pub file: String,
+    pub model: String,
+    pub prompt: Option<String>,
+    pub response_format: Option<String>,
+    pub temperature: Option<f32>,
+    pub language: Option<String>,
+}
 
-        if let Some(n) = value.get("n") {
-            config.n = Some(n.to_u64().unwrap() as usize);
-        }
+impl FromValueBehavior for AudioBody {
+    type Item = AudioBody;
 
-        if let Some(stream) = value.get("stream") {
-            config.stream = stream.as_bool().cloned();
-        }
+    fn from_value(value: Value) -> Option<Self::Item> {
+        let file = value.get("file")?.to_string();
 
-        if let Some(stop) = value.get("stop") {
-            let stop_vec: Vec<String> = stop
-                .as_array()
-                .unwrap()
-                .into_iter()
-                .map(|s| s.to_string())
-                .collect();
-            config.stop = Some(stop_vec);
-        }
+        let model = value.get("model")?.to_string();
 
-        if let Some(presence_penalty) = value.get("presence_penalty") {
-            config.presence_penalty = Some(presence_penalty.to_f64().unwrap() as f32);
-        }
+        let prompt = value.get("prompt").map(|p| p.to_string());
 
-        if let Some(frequency_penalty) = value.get("frequency_penalty") {
-            config.frequency_penalty = Some(frequency_penalty.to_f64().unwrap() as f32);
-        }
+        let response_format = value.get("response_format").map(|r| r.to_string());
 
-        if let Some(logit_bias) = value.get("logit_bias") {
-            let mut bias_map = HashMap::new();
+        let temperature = value.get("temperature").map(|t| t.to_f64().unwrap() as f32);
 
-            match logit_bias.as_object() {
-                Some(obj) => {
-                    for (k, v) in obj.iter() {
-                        bias_map.insert(k.to_string(), v.to_string());
-                    }
-                }
-                None => return Err("logit_bias must be a JSON object".to_string()),
+        let language = value.get("language").map(|l| l.to_string());
+
+        Some(AudioBody {
+            file,
+            model,
+            prompt,
+            response_format,
+            temperature,
+            language,
+        })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum OpenaiApi {
+    Completions(CompletionsBody),
+    Chat(ChatBody),
+    Images(ImagesBody),
+    ImagesEdit(ImagesEditBody),
+    Embeddings(EmbeddingsBody),
+    Audio(AudioBody),
+}
+
+impl FromValueBehavior for OpenaiApi {
+    type Item = OpenaiApi;
+
+    fn from_value(value: Value) -> Option<Self::Item> {
+        let config_type = value.as_str();
+
+        match config_type {
+            "completions" => {
+                let body = CompletionsBody::from_value(value.get("body")?.clone())?;
+                Some(OpenaiApi::Completions(body))
             }
-
-            config.logit_bias = Some(bias_map);
+            "chat" => {
+                let body = ChatBody::from_value(value.get("body")?.clone())?;
+                Some(OpenaiApi::Chat(body))
+            }
+            "images" => {
+                let body = ImagesBody::from_value(value.get("body")?.clone())?;
+                Some(OpenaiApi::Images(body))
+            }
+            "images_edit" => {
+                let body = ImagesEditBody::from_value(value.get("body")?.clone())?;
+                Some(OpenaiApi::ImagesEdit(body))
+            }
+            "embeddings" => {
+                let body = EmbeddingsBody::from_value(value.get("body")?.clone())?;
+                Some(OpenaiApi::Embeddings(body))
+            }
+            "audio" => {
+                let body = AudioBody::from_value(value.get("body")?.clone())?;
+                Some(OpenaiApi::Audio(body))
+            }
+            _ => None,
         }
-
-        if let Some(user) = value.get("user") {
-            config.user = Some(user.to_string());
-        }
-
-        Ok(config)
     }
 }
