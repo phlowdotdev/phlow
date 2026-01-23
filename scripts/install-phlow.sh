9e4acb05862915300f752f31280381fd1fb955b8
Philippe Assis <codephilippe@gmail.com>
2026-01-23 19:42:45 -0300

    scripts: download inspect cli

--- scripts/install-phlow.sh
+++ scripts/install-phlow.sh
@@ -4,8 +4,10 @@ set -e
 
 REPO="phlowdotdev/phlow"
 BIN_NAME="phlow"
+INSPECT_BIN_NAME="phlow-cli-inspect"
 INSTALL_DIR="$HOME/.phlow"
 BIN_PATH="$INSTALL_DIR/phlow"
+INSPECT_BIN_PATH="$INSTALL_DIR/phlow-cli-inspect"
 ADD_PATH_CMD='export PATH="$HOME/.phlow:$PATH"'
 
 echo "🔍 Detecting platform..."
@@ -16,13 +18,16 @@ ARCH=$(uname -m)
 # Identificando o asset correto
 if [[ "$OS" == "Darwin" ]]; then
     ASSET_NAME="${BIN_NAME}-macos"
+    INSPECT_ASSET_NAME="${INSPECT_BIN_NAME}-macos"
 elif [[ "$OS" == "Linux" ]]; then
     case "$ARCH" in
         x86_64)
             ASSET_NAME="${BIN_NAME}-amd64"
+            INSPECT_ASSET_NAME="${INSPECT_BIN_NAME}-amd64"
             ;;
         aarch64 | arm64)
             ASSET_NAME="${BIN_NAME}-arm64"
+            INSPECT_ASSET_NAME="${INSPECT_BIN_NAME}-arm64"
             ;;
         *)
             echo "❌ Unsupported Linux architecture: $ARCH"
@@ -41,13 +46,18 @@ LATEST=$(curl -s https://api.github.com/repos/$REPO/releases/latest | grep tag_n
 echo "📦 Latest version is $LATEST"
 
 URL="https://github.com/$REPO/releases/download/$LATEST/$ASSET_NAME"
+INSPECT_URL="https://github.com/$REPO/releases/download/$LATEST/$INSPECT_ASSET_NAME"
 
 echo "🚚 Downloading $BIN_NAME from $URL..."
 mkdir -p "$INSTALL_DIR"
 curl -L "$URL" -o "$BIN_PATH"
 
+echo "🚚 Downloading $INSPECT_BIN_NAME from $INSPECT_URL..."
+curl -L "$INSPECT_URL" -o "$INSPECT_BIN_PATH"
+
 echo "⚙️ Making binary executable..."
 chmod +x "$BIN_PATH"
+chmod +x "$INSPECT_BIN_PATH"
 
 echo "🔧 Updating shell configuration files..."
 for shell_rc in "$HOME/.bashrc" "$HOME/.zshrc"; do
